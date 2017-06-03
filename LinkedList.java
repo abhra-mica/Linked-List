d1fff0b5dd0ffb34fe14b668fce66f17409e0472
Abhra Kar <abhra.kar@gmail.com>
2017-06-03 22:04:32 +0530

    Add files via upload
    1> addRecursive method added
    2>add for any position added 
    3>delete last node added

--- LinkedList.java
+++ LinkedList.java
@@ -26,17 +26,63 @@ public class LinkedList {
 		}
 	}
 
-	// 2> display linked list
+	// 1a> recursively add nodes
+	void addRecursive(int data) {
+		if (root == null) {
+			root = new Node(data);
+			return;
+		}
+		Node node = root;
+		recursiveSupport(node, data);
+	}
 
+	// Suportive method for addRecursive
+	void recursiveSupport(Node node, int data) {
+		if (node.next == null) {
+			node.next = new Node(data);
+			return;
+		}
+		recursiveSupport(node.next, data);
+	}
+
+	// 1b> add node in particular position
+
+	void add(int data, int position) {
+		Node node = root;
+		int count = 0;
+		while (count != position) {
+			node = node.next;
+			count++;
+		}
+		Node temp = node.next;
+		node.next = new Node(data);
+		node.next.next = temp;
+	}
+
+	// 2> display linked list
 	public void display(Node node) {
-		
-		if(node != null)
-		System.out.print(node.data);
+		if (node != null)
+			System.out.print(node.data);
 		else
 			return;
-		if(node.next != null)
-		System.out.print(" --> ");
+		if (node.next != null)
+			System.out.print(" --> ");
 		display(node.next);
 	}
 
+	// 3> delete a node from last
+	void delete() {
+		if (root == null) {
+			System.out.println("Empty list");
+			return;
+		}
+
+		if (root.next == null)
+			root = null;
+		Node node = root;
+		while (node.next.next != null) {
+			node = node.next;
+		}
+		node.next = null;
+	}
 }
