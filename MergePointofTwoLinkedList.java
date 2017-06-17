088ad33bfbc1383dcc602592cef72479ca107a2f
Abhra Kar <abhra.kar@gmail.com>
2017-06-17 16:55:32 +0530

    Add files via upload

---
+++ MergePointofTwoLinkedList.java
@@ -0,0 +1,65 @@
+package Linked_List;
+
+import Linked_List.LinkedList.Node;
+
+public class MergePointofTwoLinkedList {
+
+	public static void main(String[] args) {
+		LinkedList<Integer> list1 = new LinkedList<>();
+		 list1.addRecursive(1);
+		 list1.addRecursive(2);
+		 list1.addRecursive(3);
+		 list1.addRecursive(4);
+		 list1.addRecursive(5);
+		 list1.addRecursive(6);
+		 
+		 System.out.println(list1.size());
+		 
+		 LinkedList<Integer> list2 = new LinkedList<>();
+		 list2.addRecursive(9);
+		 list2.addRecursive(9);
+		 list2.addRecursive(9);
+		 list2.addRecursive(9);
+		
+		 list2.head.next.next.next.next = list1.head.next;
+		 System.out.println(list2.size());
+		 
+		 Node mergePoint = mergePoint(list1, list2);
+		 if(mergePoint!= null)
+			 System.out.println(mergePoint.data);
+	}
+	
+public static	Node mergePoint(LinkedList<Integer> list1, LinkedList<Integer> list2) {
+		Node<Integer> list1Head = list1.head;
+		Node<Integer> list2Head = list2.head;
+		int count = list1.size() - list2.size(); // Get difference between two
+													// linked list
+		/*
+		 * Below if and else if blocks hand two different length of list and if
+		 * two lists are equal also and it cover the extra length list
+		 * traversal.
+		 */
+		if (count > 0) {
+			while (count != 0) {
+				list1Head = list1Head.next;
+				count--;
+			}
+
+		} else if (count < 0) {
+			while (count != 0) {
+				list2Head = list2Head.next;
+				count++;
+			}
+		}
+		// From here both list have same nodes
+		while (list1Head != null) {
+			if (list1Head.data == list2Head.data) {
+				return list1Head;
+			}
+			list1Head = list1Head.next;
+			list2Head = list2Head.next;
+		}
+
+		return null;
+	}
+}
---
+++ MergeTwoSortedLinkedList.java
@@ -0,0 +1,72 @@
+package Linked_List;
+
+import Linked_List.LinkedList.Node;
+
+public class MergeTwoSortedLinkedList {
+
+	public static void main(String[] args) {
+		LinkedList<Integer> list1 = new LinkedList<>();
+		list1.addRecursive(10);
+		list1.addRecursive(50);
+		list1.addRecursive(70);
+		list1.addRecursive(90);
+		list1.addRecursive(100);
+
+		LinkedList<Integer> list2 = new LinkedList<>();
+		list2.addRecursive(20);
+		list2.addRecursive(30);
+		list2.addRecursive(40);
+		list2.addRecursive(60);
+		list2.addRecursive(80);
+
+		Node node = mergeTwoSortedLinkedList(list1, list2);
+		list1.display(node);
+	}
+
+	static Node mergeTwoSortedLinkedList(LinkedList<Integer> list1,
+			LinkedList<Integer> list2) {
+		Node currentPtr = null;
+		Node firstListPtr = null;
+		Node secListPtr = null;
+		Node newHead = null;
+
+		/*
+		 * Setting up the new head node and first and second pointer for the
+		 * first time
+		 */
+		if ((Integer) list1.head.data < (Integer) list2.head.data) {
+			newHead = currentPtr = list1.head;
+			firstListPtr = currentPtr.next;
+			secListPtr = list2.head;
+		} else {
+			newHead = currentPtr = list2.head;
+			secListPtr = currentPtr.next;
+			firstListPtr = list1.head;
+		}
+
+		// After this loop either one of the below while loop will execute.
+		while (firstListPtr != null && secListPtr != null) {
+			if ((Integer) firstListPtr.data < (Integer) secListPtr.data) {
+				currentPtr.next = firstListPtr;
+				currentPtr = firstListPtr;
+				firstListPtr = firstListPtr.next;
+			} else if ((Integer) firstListPtr.data > (Integer) secListPtr.data) {
+				currentPtr.next = secListPtr;
+				currentPtr = secListPtr;
+				secListPtr = secListPtr.next;
+			}
+		}
+
+		while (firstListPtr != null) {
+			currentPtr.next = firstListPtr;
+			firstListPtr = firstListPtr.next;
+			currentPtr = currentPtr.next;
+		}
+		while (secListPtr != null) {
+			currentPtr.next = secListPtr;
+			secListPtr = secListPtr.next;
+			currentPtr = currentPtr.next;
+		}
+		return newHead;
+	}
+}
