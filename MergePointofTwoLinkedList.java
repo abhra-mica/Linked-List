5f524ce7fd4cd7c757b4222bd02508d4899e5abc
Abhra Kar <abhra.kar@gmail.com>
2017-09-03 16:12:27 +0530

    Update MergePointofTwoLinkedList.java

--- MergePointofTwoLinkedList.java
+++ MergePointofTwoLinkedList.java
@@ -1,9 +1,7 @@
 package Linked_List;
-
 import Linked_List.LinkedList.Node;
 
 public class MergePointofTwoLinkedList {
-
 	public static void main(String[] args) {
 		LinkedList<Integer> list1 = new LinkedList<>();
 		 list1.addRecursive(1);
@@ -29,11 +27,10 @@ public class MergePointofTwoLinkedList {
 			 System.out.println(mergePoint.data);
 	}
 	
-public static	Node mergePoint(LinkedList<Integer> list1, LinkedList<Integer> list2) {
+public static Node mergePoint(LinkedList<Integer> list1, LinkedList<Integer> list2) {
 		Node<Integer> list1Head = list1.head;
 		Node<Integer> list2Head = list2.head;
-		int count = list1.size() - list2.size(); // Get difference between two
-													// linked list
+		int count = list1.size() - list2.size(); // Get difference between two linked list											
 		/*
 		 * Below if and else if blocks hand two different length of list and if
 		 * two lists are equal also and it cover the extra length list
