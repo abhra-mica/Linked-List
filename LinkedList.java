a35bb5ecb109db6cb2bbc4cd426a8ad02bac177a
Abhra Kar <abhra.kar@gmail.com>
2021-06-27 13:34:48 +0530

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,175 +1,46 @@
-public class LinkedList<T> {
-	public  Node head;
+package LINKED_LIST;
 
-	//Inner class to represents nodes
-	public static class Node<T> {
-		T data;
-		Node next;
-		Node(T data) {
-			this.data = data;
+import LINKED_LIST.LinkedList.Node;
+
+public class DetectAndDeleteLoopFromLinkedList {
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+
+		for (int i = 1; i < 7; i++) {
+			list.add(i);
 		}
+
+		list.head.next.next.next.next.next.next = list.head.next.next.next;
+		detectAndDeleteLoop(list);
+		list.display();
 	}
 
-	//No argument constructor
-	public LinkedList() {
-		super();
-	}
+	/*
+	 * First detect loop's existence then find starting point of loop
+	 */
 
-	//Argumented constructor
-	public LinkedList(Node head) {
-		this.head = head;
-	}
-
-	// 1> add new node at the end of the linked list
-	public void add(T newData) {
-		Node temp = new Node(newData);
-		if (head == null)
-			head = temp;
-		else {
-			Node node = head;
-			while (node.next != null)
-				node = node.next;
-			node.next = temp;
+	public static void detectAndDeleteLoop(LinkedList list) {
+		Node fastPointer = list.head;
+		Node slowPointer = list.head;
+		// Detecting Loop is present or not using Hare and Tortoise Algorithm
+		while (slowPointer != null && fastPointer != null && fastPointer.next != null
+				&& fastPointer.next.next != null) {
+			slowPointer = slowPointer.next;
+			fastPointer = fastPointer.next.next;
+			if (slowPointer == fastPointer) {
+				System.out.println("Loop is present");
+				break;
+			}
 		}
-	}
+		// Find starting point of the loop using Floyed cycle algorithm
+		slowPointer = list.head;
 
-	// 1a> recursively add nodes
-	void addRecursive(T data) {
-		if (head == null) {
-			head = new Node(data);
-			return;
+		while (slowPointer.next != fastPointer.next) {
+			slowPointer = slowPointer.next;
+			fastPointer = fastPointer.next;
 		}
-		Node node = head;
-		recursiveSupport(node, data);
-	}
-
-	// Suportive method for addRecursive
-	void recursiveSupport(Node node, T data) {
-		if (node.next == null) {
-			node.next = new Node(data);
-			return;
-		}
-		recursiveSupport(node.next, data);
-	}
-
-	// 1b> add node in particular position
-	void add(T data, int position) {
-		Node node = head;
-		int count = 0;
-		while (count != position) {
-			node = node.next;
-			count++;
-		}
-		Node temp = node.next;
-		node.next = new Node(data);
-		node.next.next = temp;
-	}
-
-	// 2> display linked list
-	public static void display(Node node) {
-		if (node != null)
-			System.out.print(node.data);
-		else
-			return;
-		if (node.next != null)
-			System.out.print(" --> ");
-		display(node.next);
-	}
-
-	// 3> delete a node from last
-	void delete() {
-		if (head == null) {
-			System.out.println("Empty list");
-			return;
-		}
-
-		if (head.next == null)
-			head = null;
-		Node node = head;
-		while (node.next.next != null) {
-			node = node.next;
-		}
-		node.next = null;
-	}
-
-	// 4> Reverse a linked list in a iterative way
-	Node reverseItr(Node node) {
-		Node prev = null;
-		Node current = head;
-		Node nextEle = null;
-		while (current != null) {
-			nextEle = current.next;
-			current.next = prev;
-			prev = current;
-			current = nextEle;
-		}
-		head = prev;
-		return head;
-	}
-
-	// 5> Reverse A linked list in a recursive way
-	public  Node reverseRec(Node node) {
-		if (node.next == null) {
-			head = node;
-			return head;
-		}
-		reverseRec(node.next);
-		Node temp = node.next;
-		temp.next = node;
-		node.next = null;
-		return head;
-	}
-
-	// 6>Size of linked list
-	public int size() {
-		Node temp = head;
-		int count = 0;
-		while (temp != null) {
-			count++;
-			temp = temp.next;
-		}
-		return count;
-	}
-	
-	// 7>Swap node without swaping data
-	public void swapNodes(int x, int y) {
-		Node currX = head;
-		Node prevX = null;
-		Node currY = head;
-		Node prevY = null;
-
-		if (x == y)
-		return;
-
-		// Below two while blocks to track X,Y and it's previous elements
-		while (currX != null && (Integer) currX.data != x) {
-			prevX = currX;
-			currX = currX.next;
-		}
-		while (currY != null && (Integer) currY.data != y) {
-			prevY = currY;
-			currY = currY.next;
-		}
-
-		// If either X or Y are not present then no need to swap
-		if (currX == null || currY == null) {
-			System.out.println("Node are not available");
-			return;
-		}
-
-		// swap nodes
-		if (prevX != null)
-		    prevX.next = currY;
-		else
-		    head = currY;
-
-		if (prevY != null)
-		    prevY.next = currX;
-		else
-		    head = currX;
-		
-		Node temp = currY.next;
-		currY.next = currX.next;
-		currX.next = temp;
+		System.out.println("Start point of loop is " + slowPointer.next.data);
+		// Remove loop
+		fastPointer.next = null;
 	}
 }
